17aed2d93d872caa4f761c681f9e19ebcc80a1d2
dkagh012 <qowjdxo00@gmail.com>
2023-07-14 01:06:17 +0900

    마무리

--- src/components/Home/Swiper/SwiperPage.js
+++ src/components/Home/Swiper/SwiperPage.js
@@ -9,26 +9,24 @@ import 'swiper/css/scrollbar';
 
 function SwiperPage() {
   return (
-    <>
-      <Swiper className={classes.Swiper_Wrap}
-        // install Swiper modules
-        modules={[Navigation, Pagination, A11y]}
-        navigation
-        pagination={{
-          clickable: true,
-          el: '.swiper-pagination',
-          type: 'fraction',
-        }}
-        onSwiper={(swiper) => console.log(swiper)}
-        onSlideChange={() => console.log('slide change')}
-      >
-        <SwiperSlide className={classes.Swiper_Img}>Slide 1</SwiperSlide>
-        <SwiperSlide className={classes.Swiper_Img}>Slide 2</SwiperSlide>
-        <SwiperSlide className={classes.Swiper_Img}>Slide 3</SwiperSlide>
-        <SwiperSlide className={classes.Swiper_Img}>Slide 4</SwiperSlide>
-      </Swiper>
+    <Swiper className={classes.Swiper_Wrap}
+      // install Swiper modules
+      modules={[Navigation, Pagination, A11y]}
+      navigation
+      pagination={{
+        clickable: true,
+        el: '.swiper-pagination',
+        type: 'fraction',
+      }}
+      onSwiper={(swiper) => console.log(swiper)}
+      onSlideChange={() => console.log('slide change')}
+    >
+      <SwiperSlide className={classes.Swiper_Img}>Slide 1</SwiperSlide>
+      <SwiperSlide className={classes.Swiper_Img}>Slide 2</SwiperSlide>
+      <SwiperSlide className={classes.Swiper_Img}>Slide 3</SwiperSlide>
+      <SwiperSlide className={classes.Swiper_Img}>Slide 4</SwiperSlide>
       <div className="pagination-num"></div>
-    </>
+    </Swiper>
   );
 };
 export default SwiperPage;
