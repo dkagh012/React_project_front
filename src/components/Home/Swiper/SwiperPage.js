ca526b26a310eca42e4cd184ab0170626cead9da
dkagh012 <qowjdxo00@gmail.com>
2023-07-16 00:10:17 +0900

    작업 마무리

--- src/components/Home/Swiper/SwiperPage.js
+++ src/components/Home/Swiper/SwiperPage.js
@@ -1,32 +1,57 @@
-import { Navigation, Pagination, A11y } from 'swiper/modules';
+import { useState } from 'react';
+import { FaPlay, FaPause } from 'react-icons/fa';
+import { Navigation, Pagination, A11y, Autoplay } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import classes from './SwiperPage.module.css';
 // Import Swiper styles
+import './SwiperEvent.css';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
 
 function SwiperPage() {
+  const [isPlaying, setIsPlaying] = useState(true);
+  const [swiper, setSwiper] = useState(null);
+
+  const play = () => {
+    swiper.autoplay.start();
+    setIsPlaying(true);
+  };
+
+  const pause = () => {
+    swiper.autoplay.stop();
+    setIsPlaying(false);
+  };
+
   return (
-    <Swiper className={classes.Swiper_Wrap}
-      // install Swiper modules
-      modules={[Navigation, Pagination, A11y]}
+    <Swiper
+      className={classes.Swiper_Wrap}
+      modules={[Navigation, Pagination, A11y, Autoplay]}
       navigation
       pagination={{
         clickable: true,
-        el: '.swiper-pagination',
+        el: '#paginationNumber',
         type: 'fraction',
       }}
-      onSwiper={(swiper) => console.log(swiper)}
+      autoplay={{
+        delay: 2500,
+        disableOnInteraction: false,
+      }}
+      onSwiper={setSwiper}
       onSlideChange={() => console.log('slide change')}
     >
       <SwiperSlide className={classes.Swiper_Img}>Slide 1</SwiperSlide>
       <SwiperSlide className={classes.Swiper_Img}>Slide 2</SwiperSlide>
       <SwiperSlide className={classes.Swiper_Img}>Slide 3</SwiperSlide>
       <SwiperSlide className={classes.Swiper_Img}>Slide 4</SwiperSlide>
-      <div className="pagination-num"></div>
+      <div className={classes.Swiper_Buttons}>
+        <div className={classes.pagination_num} id='paginationNumber'></div>
+        {!isPlaying && <button className={classes.SwiperBtn} onClick={play}> <FaPlay /></button>}
+        {isPlaying && <button className={classes.SwiperBtn} onClick={pause}>  <FaPause /></button>}
+      </div>
     </Swiper>
   );
 };
+
 export default SwiperPage;
