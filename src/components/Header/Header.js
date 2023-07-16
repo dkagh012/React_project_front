c492995503638576ed5da28f8f316136f5337f13
dkagh012 <qowjdxo00@gmail.com>
2023-07-16 20:45:22 +0900

    로그인 페이지 구현하기

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, BrowserRouter } from 'react-router-dom';
 import classes from './Header.module.css';
+import Login from './Login/Login'
 import Logo from '../../assets/image/logo.png';
+
 function Header() {
+  const [showLogin, setShowLogin] = useState(false); // 로그인 팝업 표시 상태
+
+  const handleLoginClick = () => {
+    setShowLogin(!showLogin); // 로그인 팝업 표시 상태를 토글
+  };
+
   return (
     <BrowserRouter >
       <header className={classes.Header}>
@@ -21,10 +29,10 @@ function Header() {
             </ul>
           </div>
           <div className={`${classes.HeaderLoginButton} ${classes.HeaderFontStyle}`}>
-            <Link to="/">로그인/회원가입</Link>
+            <button type="button" onClick={handleLoginClick}>로그인/회원가입</button>
           </div>
-
         </div>
+        {showLogin && <Login />} {/* 로그인 팝업 표시 상태가 true일 때만 Login 컴포넌트를 렌더링 */}
       </header>
     </BrowserRouter>
   );
---
+++ src/components/Header/Login/Login.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import classes from './Login.module.css'
+import { IoClose } from "react-icons/io5";
+function Login() {
+  return (
+    <div>
+    </div>
+  )
+}
+
+export default Login;
