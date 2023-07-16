ae47a33f6589c56c49955ddc85e2ef8e66c2e082
dkagh012 <qowjdxo00@gmail.com>
2023-07-16 20:04:45 +0900

    메인페이지 마무리

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import classes from './App.module.css';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home'
-// import Footer from './components/Footer/Footer'
+import Footer from './components/Footer/Footer'
 import './reset.css'
 function App() {
   return (
     <div className={classes.Wrap}>
       <Header />
       <Home />
-      {/* <Footer /> */}
+      <Footer />
     </div>
   )
 }
