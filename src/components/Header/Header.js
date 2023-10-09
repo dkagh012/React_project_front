b322fab7873ee81d511c94beef50456ec5446244
dkagh012 <qowjdxo00@gmail.com>
2023-10-09 21:48:19 +0900

    사용자 알림 기능 추가

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,13 +4,19 @@ import classes from './Header.module.scss';
 import Login from './Login/Login'
 import Notice from './Notice/Notice'
 import Logo from '../../assets/image/logo.png';
-
+import UserIcon from '../../assets/image/companyImg.png';
+import { FaRegBell } from "react-icons/fa";
+import { TbMessageCircle2 } from "react-icons/tb";
 function Header() {
 
   const [showLogin, setShowLogin] = useState(false);
+  const [showNotice, setShowNotice] = useState(false);
 
   const handleLoginClick = () => {
     setShowLogin(!showLogin);
+  };
+  const handleAlarmClick = () => {
+    setShowNotice(!showNotice);
   };
 
   return (
@@ -32,12 +38,13 @@ function Header() {
           </ul>
         </div>
         {/* <div className={`${classes.HeaderLoginButton} ${classes.HeaderFontStyle}`}>
-          <button type="button" onClick={handleLoginClick}>로그인/회원가입</button>
+          <button className={classes.LoginBtn} type="button" onClick={handleLoginClick}>로그인/회원가입</button>
         </div> */}
         <div className={`${classes.HeaderLoginButton} ${classes.HeaderFontStyle}`}>
-          <button type="button" className={classes.chat}></button>
-          <button type="button" className={classes.notice}></button>
-          <button type="button" className={classes.UserIcon}></button>
+          <button type="button" className={classes.UserIcon}><TbMessageCircle2 /></button>
+          <button type="button" className={classes.notice} onClick={handleAlarmClick}><FaRegBell /></button>
+          <button type="button" className={classes.chat}><img src={UserIcon} alt="UserIcon" /></button>
+          {showNotice && <Notice onClose={handleAlarmClick} />}
         </div>
         {/* <Notice /> */}
       </div>
--- src/components/Header/Notice/Notice.js
+++ src/components/Header/Notice/Notice.js
@@ -1,20 +1,87 @@
 import React from "react";
 import classes from './Notice.module.scss';
-
+import { Link } from 'react-router-dom';
 function Notice() {
 
   return (
-    <div>
+    <div className={classes.NoticeBox}>
       <div className={classes.NoticeHeader}>
         <h2>알림</h2>
         <button type="button"> 모두 읽음</button>
       </div>
       <div className={classes.NoticeBody}>
+        {/* <ul className={classes.NoticeNoMessage}>
+          <h2>알림이 없습니다</h2>
+        </ul> */}
         <ul>
           <li>
-            <span>업체명업체명업체명업체명업체명업체명업체명에서나를관리자로초대를했습니다</span>
-            <h2>2023.09.05 오후 03:00</h2>
+            <Link to="">
+              <div><span className={classes.NoticeTitle}>업체명업체명업체명업체명업체명업체명업체명<span className={classes.NoticeText}>에서나를관리자로초대를했습니다</span></span></div>
+              <h2>2023.09.05 오후 03:00</h2>
+            </Link>
           </li>
+          <li>
+            <Link to="">
+              <div><span className={classes.NoticeTitle}>업체명업체명업체명업체명업체명업체명업체명<span className={classes.NoticeText}>에서나를관리자로초대를했습니다</span></span></div>
+              <h2>2023.09.05 오후 03:00</h2>
+            </Link>
+          </li>
+          <li>
+            <Link to="">
+              <div><span className={classes.NoticeTitle}>업체명업체명업체명업체명업체명업체명업체명<span className={classes.NoticeText}>에서나를관리자로초대를했습니다</span></span></div>
+              <h2>2023.09.05 오후 03:00</h2>
+            </Link>
+          </li>
+
+          <li>
+            <Link to="">
+              <div><span className={classes.NoticeTitle}>업체명업체명업체명업체명업체명업체명업체명<span className={classes.NoticeText}>에서나를관리자로초대를했습니다</span></span></div>
+              <h2>2023.09.05 오후 03:00</h2>
+            </Link>
+          </li>
+          <li>
+            <Link to="">
+              <div><span className={classes.NoticeTitle}>업체명업체명업체명업체명업체명업체명업체명<span className={classes.NoticeText}>에서나를관리자로초대를했습니다</span></span></div>
+              <h2>2023.09.05 오후 03:00</h2>
+            </Link>
+          </li>
+          <li>
+            <Link to="">
+              <div><span className={classes.NoticeTitle}>업체명업체명업체명업체명업체명업체명업체명<span className={classes.NoticeText}>에서나를관리자로초대를했습니다</span></span></div>
+              <h2>2023.09.05 오후 03:00</h2>
+            </Link>
+          </li>
+          <li>
+            <Link to="">
+              <div><span className={classes.NoticeTitle}>업체명업체명업체명업체명업체명업체명업체명<span className={classes.NoticeText}>에서나를관리자로초대를했습니다</span></span></div>
+              <h2>2023.09.05 오후 03:00</h2>
+            </Link>
+          </li>
+          <li>
+            <Link to="">
+              <div><span className={classes.NoticeTitle}>업체명업체명업체명업체명업체명업체명업체명<span className={classes.NoticeText}>에서나를관리자로초대를했습니다</span></span></div>
+              <h2>2023.09.05 오후 03:00</h2>
+            </Link>
+          </li>
+          <li>
+            <Link to="">
+              <div><span className={classes.NoticeTitle}>업체명업체명업체명업체명업체명업체명업체명<span className={classes.NoticeText}>에서나를관리자로초대를했습니다</span></span></div>
+              <h2>2023.09.05 오후 03:00</h2>
+            </Link>
+          </li>
+          <li>
+            <Link to="">
+              <div><span className={classes.NoticeTitle}>업체명업체명업체명업체명업체명업체명업체명<span className={classes.NoticeText}>에서나를관리자로초대를했습니다</span></span></div>
+              <h2>2023.09.05 오후 03:00</h2>
+            </Link>
+          </li>
+          <li>
+            <Link to="">
+              <div><span className={classes.NoticeTitle}>업체명업체명업체명업체명업체명업체명업체명<span className={classes.NoticeText}>에서나를관리자로초대를했습니다</span></span></div>
+              <h2>2023.09.05 오후 03:00</h2>
+            </Link>
+          </li>
+
         </ul>
       </div>
     </div>
