ab3e363086ef7999c5c575892e4e2bffd5acf2e8
dkagh012 <qowjdxo00@gmail.com>
2023-10-22 17:45:48 +0900

    swiper 버튼 활성화 조건문 달기 전

--- src/components/Home/company/Best_company.js
+++ src/components/Home/company/Best_company.js
@@ -6,62 +6,59 @@ import companyImg from "../../../assets/image/companyImg.png";
 import ListClickPopup from '../../PageEvent/ListClickPopup';
 import classes from './Best_company.module.scss';
 
-import { FaPlay, FaPause, FaChevronLeft, FaChevronRight } from 'react-icons/fa';
+import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
 import { Navigation, Pagination, A11y, Autoplay } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
+import './Swiper_Tag.scss';
+
 
 function Best_company() {
-  // Swiper 기능
+  const [swiperInstances, setSwiperInstances] = useState([]);
 
-  const [swiper, setSwiper] = useState(null);
-
-  useEffect(() => {
-    if (swiper) {
-      swiper.autoplay.start();
-    }
-  }, [swiper]);
-
-
-
-  const goNext = () => {
-    if (swiper) {
-      swiper.slideNext();
-    }
-  };
-
-  const goPrev = () => {
-    if (swiper) {
-      swiper.slidePrev();
-    }
-  };
-  // 랜더링 기능
   const [selectedCompany, setSelectedCompany] = useState(companies[0]);
   const [showPopup, setShowPopup] = useState(false);
   const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
 
+  const goNext = (index) => {
+    if (swiperInstances[index]) {
+      swiperInstances[index].slideNext();
+    }
+    console.log(swiperInstances[index]);
+
+  };
+
+  const goPrev = (index) => {
+    if (swiperInstances[index]) {
+      swiperInstances[index].slidePrev();
+    }
+    console.log(swiperInstances[index]);
+
+  };
 
   const handleClick = (company) => {
     setSelectedCompany(company);
   };
 
-  const handleListClick = () => {
+  const handleListClick = (index) => {
     if (isLoggedIn) {
       setShowPopup(true);
     } else {
-      // Handle what to do when not logged in and companyItem is clicked
-      // For example, you can navigate to the login page or implement another action.
+      // 로그인되지 않았을 때 companyItem을 클릭했을 때의 처리를 여기에 추가할 수 있습니다.
+      // 예를 들어, 로그인 페이지로 이동하거나 다른 작업을 구현할 수 있습니다.
     }
   };
 
-
-
+  // useEffect 내에서 상태 업데이트로 변경합니다.
+  useEffect(() => {
+    // Swiper 인스턴스를 업데이트하는 논리를 여기에 추가합니다.
+  }, [swiperInstances]);
 
   return (
-    <section className="sectionWrap container">
+    <section className="sectionWrap container" id="company">
       <ListClickPopup showPopup={showPopup} setShowPopup={setShowPopup} />
 
       <div className={classes.company_title}>
@@ -72,7 +69,8 @@ function Best_company() {
           <li key={index}>
             <button
               className={company === selectedCompany ? `${classes.clicked}` : ''}
-              type="button" onClick={() => handleClick(company)}
+              type="button"
+              onClick={() => handleClick(company)}
             >
               {company}
             </button>
@@ -83,8 +81,11 @@ function Best_company() {
         <ul className={classes.companyItem}>
           {companiesItem.map((companyItem, index) => {
             const post = posts.find((post) => post.id === index + 1);
+
+            const swiperRef = React.createRef();
+
             return (
-              <li key={index} className={classes.companyItemLink} onClick={() => handleListClick()}>
+              <li key={index} className={classes.companyItemLink} onClick={() => handleListClick(index)}>
                 <Link to="#">
                   <div className={classes.companyItemLinkImg}>
                     <img alt={companyItem} src={companyImg} />
@@ -101,25 +102,37 @@ function Best_company() {
                     </div>
                   </div>
                   <div className={classes.companyItemLinkTag}>
+                    <button
+                      className={classes.SwiperBtn}
+                      onClick={() => goPrev(index)}
+                      style={{ display: swiperInstances[index] && !swiperInstances[index].isBeginning ? 'block' : 'none' }}
+                    >
+                      <FaChevronLeft />
+                    </button>
                     <Swiper
+                      ref={swiperRef}
                       className={classes.Swiper_Wrap}
                       modules={[Navigation, Pagination, A11y, Autoplay]}
+                      slidesPerView={3}
                       navigation
-                      onSwiper={setSwiper}
+                      onSwiper={(swiper) => setSwiperInstances((prevInstances) => {
+                        const updatedInstances = [...prevInstances];
+                        updatedInstances[index] = swiper;
+                        return updatedInstances;
+                      })}
                     >
-                      <SwiperSlide className={classes.Swiper_Img}>Slide 1</SwiperSlide>
-                      <SwiperSlide className={classes.Swiper_Img}>Slide 2</SwiperSlide>
-                      <SwiperSlide className={classes.Swiper_Img}>Slide 3</SwiperSlide>
-                      <SwiperSlide className={classes.Swiper_Img}>Slide 4</SwiperSlide>
+                      <SwiperSlide className={classes.Swiper_Img}><span>Slide 1</span></SwiperSlide>
+                      <SwiperSlide className={classes.Swiper_Img}><span>Slide 2</span></SwiperSlide>
+                      <SwiperSlide className={classes.Swiper_Img}><span>Slide 3</span></SwiperSlide>
+                      <SwiperSlide className={classes.Swiper_Img}><span>Slide 4</span></SwiperSlide>
                     </Swiper>
-                    <div className={classes.Swiper_Buttons}>
-                      <button className={classes.SwiperBtn} onClick={goPrev}>
-                        <FaChevronLeft />
-                      </button>
-                      <button className={classes.SwiperBtn} onClick={goNext}>
-                        <FaChevronRight />
-                      </button>
-                    </div>
+                    <button
+                      className={classes.SwiperBtn}
+                      onClick={() => goNext(index)}
+                      style={{ display: swiperInstances[index] && !swiperInstances[index].isEnd ? 'block' : 'none' }}
+                    >
+                      <FaChevronRight />
+                    </button>
                   </div>
                 </Link>
               </li>
