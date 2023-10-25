fe8948d42f6418e1dfeddbe17b25860c8fae3fa4
dkagh012 <qowjdxo00@gmail.com>
2023-10-26 00:25:56 +0900

    Swiper 수정

--- src/components/Home/company/Best_company.js
+++ src/components/Home/company/Best_company.js
@@ -89,6 +89,10 @@ function Best_company() {
                       <SwiperSlide>Slide 2</SwiperSlide>
                       <SwiperSlide>Slide 3</SwiperSlide>
                       <SwiperSlide>Slide 4</SwiperSlide>
+                      <SwiperSlide>Slide 1</SwiperSlide>
+                      <SwiperSlide>Slide 2</SwiperSlide>
+                      <SwiperSlide>Slide 3</SwiperSlide>
+                      <SwiperSlide>Slide 4</SwiperSlide>
                     </Swiper>
                   </div>
                 </Link>
