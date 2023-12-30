9acccded1dd72b1f326fe2dfb2fd38c674df124f
jungtae <qowjdxo00@gmail.com>
2023-12-31 02:25:09 +0900

    캘린더 작업

--- src/components/UserLog/UserLog.js
+++ src/components/UserLog/UserLog.js
@@ -6,13 +6,24 @@ import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
 import moment from "moment";
 import { IoCalendarClearOutline } from "react-icons/io5";
+import "./Calendar.css";
+
 function UserLog() {
-  const [value, onChange] = useState(new Date());
+  const [value, setValue] = useState(null);
   const [BeforeAction, setBeforeAction] = useState(false);
+
+  const handleCalendarChange = (date) => {
+    setValue(date);
+    setBeforeAction(false);
+  };
 
   function handleListOpen(element) {
     element((prev) => !prev);
   }
+  const handleReset = () => {
+    setValue(null);
+  };
+
   return (
     <div>
       <Header />
@@ -20,22 +31,56 @@ function UserLog() {
         <div className={classes.UserLogLeft}>
           <div className={classes.UserLogLeftTitle}>
             <h1>기간 필터</h1>
-            <button type="button">초기화</button>
+            <button type="button" onClick={handleReset}>
+              초기화
+            </button>
           </div>
           <div className={classes.CalenderBeforeBox}>
             {BeforeAction && (
               <Calendar
                 className={classes.CalenderBefore}
-                onChange={onChange}
-                value={value}
-              ></Calendar>
+                onChange={handleCalendarChange}
+                value={value || new Date()} // Set the initial value to null or a default date
+                formatDay={(locale, date) =>
+                  date.toLocaleString("en", { day: "numeric" })
+                }
+              />
             )}
-            <div>
+            <div className={classes.CalendarInputBox}>
               <IoCalendarClearOutline />
               <input
                 className={classes.CalendarBeforeInput}
                 onClick={() => handleListOpen(setBeforeAction)}
-                value={moment(value).format("YYYY년 MM월 DD일")}
+                value={
+                  value
+                    ? moment(value).format("YYYY년 MM월 DD일")
+                    : "년, 월, 일"
+                }
+              />
+              <p>부터</p>
+            </div>
+          </div>
+          <div className={classes.CalenderBeforeBox}>
+            {BeforeAction && (
+              <Calendar
+                className={classes.CalenderBefore}
+                onChange={handleCalendarChange}
+                value={value || new Date()} // Set the initial value to null or a default date
+                formatDay={(locale, date) =>
+                  date.toLocaleString("en", { day: "numeric" })
+                }
+              />
+            )}
+            <div className={classes.CalendarInputBox}>
+              <IoCalendarClearOutline />
+              <input
+                className={classes.CalendarBeforeInput}
+                onClick={() => handleListOpen(setBeforeAction)}
+                value={
+                  value
+                    ? moment(value).format("YYYY년 MM월 DD일")
+                    : "년, 월, 일"
+                }
               />
               <p>부터</p>
             </div>
